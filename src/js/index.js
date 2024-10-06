3edeb181f7027dd60cd3ee0ecce3bde75862e4d8
lauraosallor <lauraosallor@gmail.com>
2024-10-06 12:59:05 +0200

    estructura basica

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,22 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
+
+const startButtonElement = document.getElementById('start-button');
+const checkButtonElement = document.getElementById('check-button');
+
+
+const generateClues = () => {
+
+}
+
+const solveBoard = clues => {
+    
+}
+
+const emptyBoard = solution => {
+
+}
+
+const checkAnswer = answer => {
+    
+}
